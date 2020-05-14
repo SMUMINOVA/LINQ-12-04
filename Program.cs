cfdb8cdd9fb1a21b9e35dd1b3b5b43b56d109d71
Sobira <sobira204@gmail.com>
2020-05-14 14:59:01 +0500

    third punkt is done

--- Program.cs
+++ Program.cs
@@ -8,20 +8,30 @@ namespace LINQ
         static void Main(string[] args)
         {
             string s = "hello";
-            char[] c = s.ToCharArray();
-            var m = c.Select(x => Converter(x));          
+            var m = s.ToCharArray().Select(x => Converter(x));          
             foreach(var v in m)
             Console.Write(v);
             System.Console.WriteLine();
             
             
             s = "h3 th2r2";
-            c = s.ToCharArray();
-            m = c.Select(x => Converter(x));
+            m = s.ToCharArray().Select(x => Converter(x));
             foreach(var v in m)
             Console.Write(v);
-
-            
+            System.Console.WriteLine();
+            s = "fdghkj234gh54dfg*23fgh42";
+            m = s.ToCharArray().Where(x => (x == '1'|| x == '2'|| x == '3'||x == '4'||x == '5'||x == '6'||x == '7'||x == '8'||x == '9'||x == '*'||x == '-'||x == '+'||x == '/')).Select(x => x);
+            char action = m.First(x => (x == '*'||x == '-'||x == '+'||x == '/'));
+            var n1 = double.Parse(m.TakeWhile(x => x != action).ToArray());
+            var n = (m.SkipWhile(x => x != action)).ToList<char>();
+            n.RemoveAt(0);
+            var n2 = double.Parse(n.ToArray());
+            switch(action){
+                case '+': System.Console.WriteLine(n1 + n2); break;
+                case '-': System.Console.WriteLine(n1 - n2); break;
+                case '*': System.Console.WriteLine(n1 * n2); break;
+                case '/': System.Console.WriteLine(n1 / n2); break;
+            }
         }
         public static char Converter(char s){
             switch(s){
